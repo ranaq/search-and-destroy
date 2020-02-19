cba667dddc2f520a59f3fdd08dee7b6aaf3dcc34
Rana Quadri <rq59@ymail.com>
2020-02-19 15:59:55 -0500

    completed binarySearch

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,8 +1,55 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	
+// const binarySearch = (array, target) => {
+//   if (array.length === 0) {
+//     return false;
+//   }
+//   if (array.length === 1) {
+//     return array[0] === target;
+//   }
+//   let half = Math.floor(array.length / 2);
+//   if (array[half] === target) {
+//     return true;
+//   } else {
+//     if (target >= array[half]) {
+//       let half1 = array.slice(half, array.length);
+//       return binarySearch(half1, target);
+//     } else {
+//       let half2 = array.slice(0, half);
+//       return binarySearch(half2, target);
+//     }
+//   }
+// };
+
+const binarySearch = (array, target, start = 0, end = array.length - 1) => {
+  setTimeout(() => {
+    console.log('start (at start)', start, 'end', end);
+  }, 500);
+  if (array.length === 0) {
+    return false;
+  }
+  if (end - start === 1) {
+    return array[end] === target || array[start] === target;
+  }
+  // console.log('start', start, 'end', end);
+  setTimeout(() => {
+    console.log('start', start, 'end', end);
+  }, 500);
+  let half = Math.floor((end - start) / 2) + start;
+  if (array[half] === target) {
+    return true;
+  } else {
+    if (target > array[half]) {
+      // console.log('target', target, 'array at half', array[half]);
+      setTimeout(() => {
+        console.log('target', target, 'array at half', array[half]);
+      }, 500);
+      return binarySearch(array, target, half, end);
+    } else {
+      return binarySearch(array, target, start, half);
+    }
+  }
 };
 
 /*
@@ -14,4 +61,4 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
