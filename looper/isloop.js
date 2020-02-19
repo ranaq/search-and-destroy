1653e85b498ea4e5be35c5646d111ece77e53f3b
Rana Quadri <rq59@ymail.com>
2020-02-19 17:23:11 -0500

    Completed looper and min-jumps

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,19 @@
 'use strict';
 
 //Complete this algo
-const isLoop = (linkedlist) => {
-
+const isLoop = linkedlist => {
+  let store = {};
+  let currNode = linkedlist.head;
+  // console.log('currNode', currNode);
+  while (currNode.next) {
+    if (store[currNode.next.value]) {
+      return currNode.next.value;
+    }
+    store[currNode.value] = true;
+    currNode = currNode.next;
+  }
+  return false;
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +23,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,26 @@
 
 // Complete this algo
 const minJumps = arr => {
-
+  let totalJumps = 0;
+  let pointer = 0;
+  let jumpLength;
+  if (arr.length <= 1) {
+    return 0;
+  }
+  if (arr[pointer] >= arr.length - 1) return 1;
+  while (pointer < arr.length - 1) {
+    if (arr[pointer] + pointer >= arr.length - 1) return totalJumps + 1;
+    let maxIdx = 0;
+    jumpLength = arr[pointer];
+    for (let i = pointer + 1; i < jumpLength + pointer + 1; i++) {
+      if (arr[i] > arr[maxIdx]) {
+        maxIdx = i;
+      }
+    }
+    pointer = maxIdx;
+    totalJumps++;
+  }
+  return totalJumps;
 };
 
-module.exports = minJumps
+module.exports = minJumps;
